c374008132e9031f8d193e41cac1fb2e1ca8db85
neloduka-sobe <81869827+neloduka-sobe@users.noreply.github.com>
2022-02-09 17:20:28 +0100

    Updated main.py
    Added command line parameters and debug mode

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import math
+import sys
 
 # Constants
-PIN_NUMBER = 23
 RADIUS = 0.037 # in meters
 SIGNALS_PER_REVOLUTION = 4 # signals received from hardware per one revolution
 
@@ -21,23 +21,55 @@ def increase_counter(channel):
     global counter
     counter += 1
 
+def print_help(program_name):
+    """Prints help for the user"""
+    print(f"{program_name} <GPIO-pin> <path-to-csv-file> [debug]")
+    print("Usage: ")
+    print(f"{program_name} 5 /path/to/csvfile")
+    print(f"{program_name} 5 /path/to/csvfile debug")
+    sys.exit(1)
+
+def print_debug(rpm, speed, date):
+    """Prints debug information"""
+    print(f"RPM is {rpm}")
+    print(f"Speed is equal to {speed} m/s")
+    print(f"Date is {date}")
+
+# Getting GPIO pin and /path/to/file from sys.argv()
+try:
+    pin_number = int(sys.argv[1])
+    path_to_file = sys.argv[2]
+    if len(sys.argv) == 4:
+        debug = "debug" == sys.argv[3]
+except:
+    print_help(sys.argv[0])
+
 # Setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN_NUMBER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(PIN_NUMBER, GPIO.FALLING, callback=increase_counter)
+GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(pin_number, GPIO.FALLING, callback=increase_counter)
 
 # Main loop
 try:
     while True:
+        # Waiting for data to be collected
         time.sleep(60)
-        print(f"RPM is {counter/SIGNALS_PER_REVOLUTION}")
-        # calculating speed of wind
+
+        # Calculating speed of wind, rpm and saving date into variables
         speed = (2 * math.pi * RADIUS * (counter/SIGNALS_PER_REVOLUTION)) / 60
-        print(f"Speed is equal to {speed} m/s")
-        print("Writing data into wind_speed.csv")
-        with open("wind_speed.csv", "a") as f:
-            f.writelines(f"{datetime.now()}, {speed}, {counter/SIGNALS_PER_REVOLUTION}\n")
+        rmp = counter/SIGNALS_PER_REVOLUTION
+        date = datetime.now()
+
+        # Printing debug info
+        if debug:
+            print_debug(rpm, speed, date)
+
+        # Writing data into the file
+        with open(path_to_file, "a") as f:
+            f.writelines(f"{date}, {speed}, {rpm}\n")
+
         counter = 0
 except:
     # Cleaning ports
     GPIO.cleanup()
+    sys.exit(0)
